a983841962e9b514df8af612628ef970f68a8497
MarkPThomas <markums@gmail.com>
2016-11-06 12:47:56 -0800

    Updated project references
    Classes extracted to other libraries. References to these added.

--- Sierra-Guidebook/Model/DirectorySyncer.cs
+++ Sierra-Guidebook/Model/DirectorySyncer.cs
@@ -6,6 +6,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 
+using MPT.Excel;
+using MPT.Trees;
+
 namespace Sierra_Guidebook
 {
     public class DirectorySyncer
--- Sierra-Guidebook/Model/ParentRegion.cs
+++ Sierra-Guidebook/Model/ParentRegion.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using MPT.Trees;
+
 namespace Sierra_Guidebook
 {
     class ParentRegion :  BaseRegionDirectory
