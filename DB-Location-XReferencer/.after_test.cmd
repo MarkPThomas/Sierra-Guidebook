42f2edf3c1f7112e256c77d4573c7afdd3342c89
MarkPThomas <markums@gmail.com>
2018-05-11 20:59:10 -0600

    AppVeyor to Coveralls
    Switching slashes for Windows Path in exectutable calls.

--- DB-Location-XReferencer/.after_test.cmd
+++ DB-Location-XReferencer/.after_test.cmd
@@ -2,6 +2,6 @@ nuget install NUnit.Runners -Version 2.6.4 -OutputDirectory DB-Location-XReferen
 nuget install OpenCover -Version 4.6.519 -OutputDirectory DB-Location-XReferencer/tools
 nuget install coveralls.net -Version 0.412.0 -OutputDirectory DB-Location-XReferencer/tools
   
-DB-Location-XReferencer/tools/OpenCover/4.6.519/tools/OpenCover.Console.exe -target:"./tools/NUnit/Runners/2.6.4/tools/nunit-console.exe" -targetargs:"/nologo /noshadow ./DB-Location-XReferences.Tests/bin/Debug/DB_Location_XReferences.Tests.dll" -filter:"+[] -[.Tests]" -register:user
+.\DB-Location-XReferencer\tools\OpenCover\4.6.519\tools\OpenCover.Console.exe -target:"./tools/NUnit/Runners/2.6.4/tools/nunit-console.exe" -targetargs:"/nologo /noshadow ./DB-Location-XReferences.Tests/bin/Debug/DB_Location_XReferences.Tests.dll" -filter:"+[] -[.Tests]" -register:user
  
-DB-Location-XReferencer/tools/coveralls.net/0.412/tools/csmacnz.Coveralls.exe --opencover -i .results.xml
+.\DB-Location-XReferencer\tools\coveralls.net\0.412\tools\csmacnz.Coveralls.exe --opencover -i .results.xml
