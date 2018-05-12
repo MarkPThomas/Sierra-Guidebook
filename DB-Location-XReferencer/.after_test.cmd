383576a4955de131d22de770f0a38a04b2636714
MarkPThomas <markums@gmail.com>
2018-05-11 20:49:45 -0600

    Fixing errors in .after_test.cmd
    WordPress article seems to be missing spaces, colons, quotes, and directory slashes.

--- DB-Location-XReferencer/.after_test.cmd
+++ DB-Location-XReferencer/.after_test.cmd
@@ -2,6 +2,6 @@ nuget install NUnit.Runners -Version 2.6.4 -OutputDirectory tools
 nuget install OpenCover -Version 4.6.519 -OutputDirectory tools
 nuget install coveralls.net -Version 0.412.0 -OutputDirectory tools
   
-.toolsOpenCover.4.6.519toolsOpenCover.Console.exe -target.toolsNUnit.Runners.2.6.4toolsnunit-console.exe -targetargsnologo noshadow .DB-Location-XReferences.TestsbinDebugDB_Location_XReferences.Tests.dll -filter+[] -[.Tests] -registeruser
+./tools/OpenCover/4.6.519/tools/OpenCover.Console.exe -target:"./tools/NUnit/Runners/2.6.4/tools/nunit-console.exe" -targetargs:"/nologo /noshadow ./DB-Location-XReferences.Tests/bin/Debug/DB_Location_XReferences.Tests.dll" -filter:"+[] -[.Tests]" -register:user
  
-.toolscoveralls.net.0.412toolscsmacnz.Coveralls.exe --opencover -i .results.xml
+.tools/coveralls.net/0.412/tools/csmacnz.Coveralls.exe --opencover -i .results.xml
