f05e3c3a888b024d98b95d52ce1b5ce808624c42
MarkPThomas <markums@gmail.com>
2018-05-11 21:29:51 -0600

    AppVeyor to Coveralls
    Tweaking path to results.xml

--- DB-Location-XReferencer/.after_test.cmd
+++ DB-Location-XReferencer/.after_test.cmd
@@ -4,4 +4,4 @@ nuget install coveralls.net -Version 0.412.0 -OutputDirectory DB-Location-XRefer
   
 .\DB-Location-XReferencer\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:".\DB-Location-XReferencer\tools\NUnit.Runners.2.6.4\tools\nunit-console.exe" -targetargs:"/nologo /noshadow .\DB-Location-XReferencer\DB-Location-XReferences.Tests\bin\Debug\DB_Location_XReferences.Tests.dll" -filter:"+[*]* -[*.Tests]*" -register:user
  
-.\DB-Location-XReferencer\tools\coveralls.net.0.412\tools\csmacnz.Coveralls.exe --opencover -i .\DB-Location-XReferencer\.results.xml
+.\DB-Location-XReferencer\tools\coveralls.net.0.412\tools\csmacnz.Coveralls.exe --opencover -i .\DB-Location-XReferencer\results.xml
