3178858d75eb3707ccc124a82ff5c5fc9395501c
MarkPThomas <markums@gmail.com>
2018-05-12 19:52:12 -0600

    Integration of SonarCoud with AppVeyor

---
+++ DB-Location-XReferencer/.build_script.cmd
@@ -0,0 +1,3 @@
+nuget install NUnit.Runners -Version 2.6.4 -OutputDirectory ./DB-Location-XReferencer/tools
+nuget install OpenCover -Version 4.6.519 -OutputDirectory ./DB-Location-XReferencer/tools
+nuget install coveralls.net -Version 0.412.0 -OutputDirectory ./DB-Location-XReferencer/tools
